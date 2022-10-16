b5042a41c648c51338094c357b7160ee4d9f8cf5
Michalzard <miso.platko@gmail.com>
2022-10-16 05:52:23 +0200

    accept now works correctly,you cannot add yourself as a friend,added decline/cancel

--- src/server/routes/friends.js
+++ src/server/routes/friends.js
@@ -4,10 +4,12 @@ const User = require("../schemas/user");
 const router = express.Router();
 const {parseCookie,checkForSession}  = require("../utils");
 
-// GET -> requests
-// POST -> request -> pending state , send info to recipient
-// POST -> accept -> accepted state,remove request object , send info to requester
-// POST -> block -> blocked state,remove request object, hold id in blocked array 
+// GET / -> friend list
+// GET /requests -> friend requests
+// POST /request -> pending state , send info to recipient
+// PATCH /accept -> accepted state on fr
+// PATCH /decline -> declined state on fr
+// PATCH /cancel -> remove whole friend request object
 
 router.get("/",async (req,res)=>{
 try{
@@ -22,10 +24,9 @@ try{
         res.status(401).send({message:"Unauthorized"});
     }
     }catch(err){
-    res.status(500).send(err);
+    res.status(500).send(err.message);
     }
 })
-
 
 router.get("/requests",async (req,res)=>{
     try{  
@@ -57,7 +58,7 @@ router.get("/requests",async (req,res)=>{
     }
     }catch(err){
         console.log(err);
-        res.status(500).send(err);
+        res.status(500).send(err.message);
     }
 })
 
@@ -70,10 +71,11 @@ router.post("/request", async (req,res)=>{
             const {user_id} = session.session;
             if(username){
             const recipient = await User.findOne({username});
-            if(!recipient){
-                return res.status(404).send({message:"Seems like this user doesn't exist"});
-            }
-            const frExists = await FriendRequest.findOne({requester:user_id,recipient:recipient._id});
+            if(!recipient) return res.status(404).send({message:"Seems like this user doesn't exist"});
+            //comparing mongoose objectIds to determine if requester is the same as user he wants to befriend
+            if(recipient._id.toString() === user_id.toString()) return res.status(404).send({message:"You cannot send friend request to yourself"});
+            
+            const frExists = await FriendRequest.findOne({requester:user_id,recipient:recipient._id,status:"pending"});
             if(frExists){
                 res.status(200).send({message:`Friend request for ${recipient.username} is already pending`});
             }else {
@@ -94,7 +96,7 @@ router.post("/request", async (req,res)=>{
         }
     }catch(err){
         console.log(err);
-        res.status(500).send(err);
+        res.status(500).send(err.message);
     }       
 })
 
@@ -109,10 +111,11 @@ router.patch("/accept",async(req,res)=>{
             const recipient = await User.findById(user_id);
             const friendRequest = await FriendRequest.findOne({recipient:user_id,requester:requester._id,status:"pending"}).populate("recipient").populate("requester");
             if(friendRequest){
-                const accepted = recipient.acceptFriend(requester._id);
-                friendRequest.status = "accepted";
+                recipient.acceptFriend(requester._id);
+                requester.acceptFriend(recipient._id);
+                const isAccepted = friendRequest.accept();
                 friendRequest.save();
-                if(accepted) res.status(200).send({message:"Friend request accepted"});
+                if(isAccepted)  res.status(200).send({message:"Friend request accepted"});
                 else res.status(200).send({message:"Friend request already accepted"});
             }else{
                 res.status(200).send({message:"Friend request no longer pending"});
@@ -121,24 +124,59 @@ router.patch("/accept",async(req,res)=>{
         res.status(401).send({message:"Unauthorized"});
     }
     }catch(err){
-    res.status(500).send(err);
+    res.status(500).send(err.message);
     }
 });
 
-router.post("/decline",async (req,res)=>{
+router.patch("/decline",async (req,res)=>{
 try{
     const cookie = parseCookie(req.headers.cookie);
     const {requesterId} = req.body;
     if(cookie){
         const session = await checkForSession(cookie.session_id);
         const {user_id} = session.session;
+        const requester = await User.findOne({id:requesterId});
+        if(!requester) return res.status(404).send({message:"Bad Request"}); 
+        const pendingRequest = await FriendRequest.findOne({requester:requester._id,recipient:user_id});        
+        if(pendingRequest){
+            const isDeclined = pendingRequest.decline();
+            pendingRequest.save();
+            if(isDeclined) res.status(200).send({message:"Friend request declined"});  
+            else  res.status(200).send({message:"Friend request was already declined"});  
+        }else{
+            res.status(200).send({message:"Friend request is no longer pending"});
+        }
+
     }else{
         res.status(401).send({message:"Unauthorized"});
     }
 }catch(err){
-res.status(500).send(err);
+res.status(500).send(err.message);
 }
 });
 
+router.patch("/cancel",async (req,res)=>{
+    try{
+        const cookie = parseCookie(req.headers.cookie);
+        console.log(cookie)
+        const {recipientId} = req.body;
+        if(cookie){
+            const session = await checkForSession(cookie.session_id);
+            const {user_id} = session.session;
+            const recipient = await User.findOne({id:recipientId});
+            const pendingRequest = await FriendRequest.findOneAndDelete({requester:user_id,recipient:recipient._id,status:"pending"});        
+            if(pendingRequest){
+               res.status(200).send({message:"Outgoing friend request cancelled"});
+            }else{
+                res.status(200).send({message:"Friend request is no longer pending"});
+            }
+    
+        }else{
+            res.status(401).send({message:"Unauthorized"});
+        }
+    }catch(err){
+    res.status(500).send(err.message);
+    }
+});
 
 module.exports = router;
