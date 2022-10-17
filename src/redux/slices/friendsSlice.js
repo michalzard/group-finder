c27232e6128ce3b129f64540fff3696995777242
Michalzard <miso.platko@gmail.com>
2022-10-17 20:45:48 +0200

    settingFriendById used to set object currentFriend which is used for sending messages to

--- src/redux/slices/friendsSlice.js
+++ src/redux/slices/friendsSlice.js
@@ -15,95 +15,92 @@ const friendsSlice = createSlice({
     loading: false,
     error: "",
     success: "",
-    friendChatId:"",
+    currentFriend:{},
   },
   reducers:{
-    setFriendChatId:(state,action)=>{
-      const {id} = action.payload;
-      //get id check friend list and grab frend object to this state
-      state.friendChatId = id;
-    }
+    setFriendById:(state,{payload})=>{
+    const index = state.list.map(friend=>friend.id).indexOf(payload.id);
+    const friend = state.list[index];
+    state.currentFriend = friend;
+    },
   },
   extraReducers: {
     //Friend requests
     [PendingFriendsRequests.pending]: (state) => {
       state.loading = true;
     },
-    [PendingFriendsRequests.fulfilled]: (state, action) => {
+    [PendingFriendsRequests.fulfilled]: (state, {payload}) => {
       state.loading = false;
-      const { requests } = action.payload;
-      state.requests = requests;
+      state.requests = payload.requests;
       state.error = "";
     },
-    [PendingFriendsRequests.rejected]: (state, action) => {
+    [PendingFriendsRequests.rejected]: (state, {payload}) => {
       state.loading = false;
-      state.error = action.payload;
+      state.error = {payload}.payload;
       state.success = "";
     },
     //All Friends
     [LoadAllFriends.pending]: (state) => {
       state.loading = true;
     },
-    [LoadAllFriends.fulfilled]: (state, action) => {
+    [LoadAllFriends.fulfilled]: (state, {payload}) => {
       state.loading = false;
-      const { friends } = action.payload;
       //expecting friends from payload to be array
-      state.list = friends;
+      state.list = payload.friends;
       state.error = "";
     },
-    [LoadAllFriends.rejected]: (state, action) => {
+    [LoadAllFriends.rejected]: (state, {payload}) => {
       state.loading = false;
-      state.error = action.payload;
+      state.error = payload;
       state.success = "";
     },
     //Add Friend (Send friend request)
     [AddFriend.pending]: (state) => {
       state.loading = true;
     },
-    [AddFriend.fulfilled]: (state, action) => {
+    [AddFriend.fulfilled]: (state, {payload}) => {
       state.loading = false;
-      const { message } = action.payload;
-      state.success = message;
+      state.success = payload.message;
       state.error = "";
     },
-    [AddFriend.rejected]: (state, action) => {
+    [AddFriend.rejected]: (state, {payload}) => {
       state.loading = false;
-      state.error = action.payload;
+      state.error = payload;
       state.success = "";
     },
     //Accept Friend Request
     [AcceptFriendRequest.pending]: (state) =>{
       state.loading = true;
     },
-    [AcceptFriendRequest.fulfilled]: (state,action) =>{
+    [AcceptFriendRequest.fulfilled]: (state,{payload}) =>{
       state.loading=false;
       state.error="";
-      state.success = action.payload;
+      state.success = payload;
       //TODO:return a friend object ???
     },
-    [AcceptFriendRequest.rejected]: (state,action) =>{
+    [AcceptFriendRequest.rejected]: (state,{payload}) =>{
       state.loading=false;
-      state.error=action.payload;
+      state.error=payload;
       state.success = "";
     },
     //Decline Friend Request
     [DeclineFriendRequest.pending]: (state) =>{
       state.loading = true;
     },
-    [DeclineFriendRequest.fulfilled]: (state,action) =>{
+    [DeclineFriendRequest.fulfilled]: (state,{payload}) =>{
       state.loading=false;
       state.error="";
-      state.success = action.payload;
+      state.success = payload;
       //TODO:return a friend object ???
     },
-    [DeclineFriendRequest.rejected]: (state,action) =>{
+    [DeclineFriendRequest.rejected]: (state,{payload}) =>{
       state.loading=false;
-      state.error=action.payload;
+      state.error=payload;
       state.success = "";
     }
   },
 });
 
-export const {setFriendChatId} = friendsSlice.actions;
+export const {setFriendById} = friendsSlice.actions;
 
 export default friendsSlice.reducer;
