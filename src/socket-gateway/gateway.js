ca960417142fb9cb8fa069dfd60054472cd98677
MichalZard <miso.platko@gmail.com>
2022-10-18 02:23:45 +0200

    changed dm emit to be volatile,dont care if messages goes trough,usually it doesnt if one of sockets isnt connect and i want to avoid messages being stuck in buffer waiting for that "other side" to connect

--- src/socket-gateway/gateway.js
+++ src/socket-gateway/gateway.js
@@ -8,21 +8,25 @@ const io = new Server(process.env.GATEWAY_PORT, {
 });
 
 const connectedUsers = [];
-
 io.on("connection", (socket) => {
-    const {id,username} = socket.handshake.auth;
+  try{
+    const {id} = socket.handshake.auth;
     if(id) connectedUsers[id] = socket.id;
     else socket.disconnect();
 
+    //TODO: send presence update directly to connected user to let him know 
+    //which of his friends are online
 
     socket.on("disconnect",()=>{
         delete connectedUsers[id];
     })
 
     socket.on("DIRECT_MESSAGE", (data) => {
-        const {to,message} = data;
+        const {to,content} = data;
         const friendSocket = connectedUsers[to];
-        socket.to(friendSocket).emit("DM_RECEIVED", {username,message});
+        socket.to(friendSocket).volatile.emit("DM_RECEIVED", {userId:id,content});
       });
-
+    }catch(err){
+      console.log(err);
+    }
 });
