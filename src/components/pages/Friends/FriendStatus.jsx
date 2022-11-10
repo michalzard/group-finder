6a5fc884c97409026b7a4635538ee8f218a9c745
MichalZard <miso.platko@gmail.com>
2022-11-11 00:27:32 +0100

    refactor

--- src/components/pages/Friends/FriendStatus.jsx
+++ src/components/pages/Friends/FriendStatus.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Alert, Button, CircularProgress, LinearProgress, Snackbar, SwipeableDrawer, TextField, Typography, useMediaQuery } from "@mui/material";
+import { Alert, Button, CircularProgress, LinearProgress, Snackbar, TextField, Typography, useMediaQuery } from "@mui/material";
 import "./FriendStatus.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { AddFriend } from "../../../redux/reducers/friendsReducers";
@@ -9,19 +9,30 @@ import FriendDms from "./FriendDms";
 import { useFormik } from "formik";
 import * as yup from "yup";
 import GroupIcon from "@mui/icons-material/Group";
-import GroupsIcon from '@mui/icons-material/Groups';
-import ListIcon from '@mui/icons-material/List';
-import { FriendListSidebar } from "./Friends";
 
 function FriendStatus() {
   const {display} = useParams();
   const navigate = useNavigate();
-  const {id} = useParams();
+
+  const isMobile = useMediaQuery("(max-width:600px)");
+  const {list,presence} = useSelector(state=>state.friends);
+
+  const handleNumber = (number,max)=>{
+    if(!max) return number;
+    if(number < max) return number;
+    else return `${max}+`;
+  }
+
+  const friendsOnline=()=>{
+    //eslint-disable-next-line
+    const onlineFriends = list.map(friend=>{if(presence.includes(friend.id)){return friend }}).filter(f=>{return f});
+    return onlineFriends.length > 0 ? onlineFriends.length : "";
+  }
 
   const renderStatus = (display)=>{
     switch(display){
       case "all" : return <AllFriends/>
-       case "online" : return <OnlineFriends/>
+       case "online" : return <OnlineFriends amount={friendsOnline()} handleNumber={handleNumber}/>
        case "pending" : return  <AllPendingRequests/>
        case "blocked" : return <BlockedFriends/>
        case "add" : return <AddFriendField/>
@@ -31,19 +42,16 @@ function FriendStatus() {
   const isSelected = (buttonName)=>{
     return display === buttonName ? "selected" : null;
   }
-  const isMobile = useMediaQuery("(max-width:600px)");
-  
-  const [drawerAnchor,setDrawerAnchor] = useState(false);
+
 
   return (
     <div className="friend-status">
-      
       <nav className="friend-nav">
       {
         isMobile ? null : <Typography className="title" variant="button"><GroupIcon /> Friends</Typography>
       }
-      <Button variant="text" className={isSelected("all")} onClick={()=>{navigate("/friends/all")}}>All</Button>
-      <Button variant="text" className={isSelected("online")} onClick={()=>{navigate("/friends/online")}}>Online</Button>
+      <Button variant="text" className={isSelected("all")} onClick={()=>{navigate("/friends/all")}}>All {handleNumber(list.length,99)}</Button>
+      <Button variant="text" className={isSelected("online")} onClick={()=>{navigate("/friends/online")}}>Online {handleNumber(friendsOnline(),99)} </Button>
       <Button variant="text" className={isSelected("pending")} onClick={()=>{navigate("/friends/pending")}}>Pending</Button>
       <Button variant="text" className={isSelected("blocked")} onClick={()=>{navigate("/friends/blocked")}}>Blocked</Button>
       <Button variant="contained" color="success" size={`${isMobile ?"small" : null}`} className="addFriend" onClick={()=>{navigate("/friends/add")}}>Add Friend</Button>
@@ -54,17 +62,6 @@ function FriendStatus() {
         renderStatus(display)
        }
       </section>
-      {isMobile ?
-      <nav className="friend-bottom-nav"> 
-      <ListIcon onClick={(e)=>{setDrawerAnchor(e.currentTarget);}}/> 
-      <GroupsIcon className={`${id ? null : "selected"}`}/>
-      </nav>
-       : null
-      }
-       
-       <SwipeableDrawer className="drawer-friendlist" anchor={"left"} open={Boolean(drawerAnchor)} onOpen={()=>{setDrawerAnchor(true);}} onClose={()=>{setDrawerAnchor(false)}}>
-       <FriendListSidebar/>
-       </SwipeableDrawer>
     </div>
   );
 }
@@ -83,8 +80,9 @@ function AllFriends(){
     <Typography variant="subtitle2" color="lightgray">All Friends - {list.length}</Typography>
     <hr/>
     {
-      list.map(friend=>{return <FriendDms key={friend.id} id={friend.id} username={friend.username} bio={friend.bio}/>})
+      list.map(friend=>{return <FriendDms key={friend.id} friend={friend}/>})
     }
+
     </>
     }
     
@@ -111,18 +109,20 @@ function AllPendingRequests(){
   )
 }
 
-function OnlineFriends(){
-  const {list,loading} = useSelector(state=>state.friends);
+function OnlineFriends({amount,handleNumber}){
+  const {list,presence,loading} = useSelector(state=>state.friends);
   //filter out list to only display online status after its added
   return(
     <>
     {
     loading ? <LinearProgress/> :
     <>
-    <Typography variant="subtitle2" color="lightgray">Online Friends - {list.length}</Typography>
+    <Typography variant="subtitle2" color="lightgray">Online Friends - {handleNumber(amount > 0 ? amount : 0,99)}</Typography>
     <hr/>
     {
-      list.map(friend=>{return <FriendDms key={friend.id} username={friend.username} bio={friend.bio}/>})
+      
+    //eslint-disable-next-line
+      list.map(friend=>{if(presence.includes(friend.id)){return <FriendDms key={friend.id} friend={friend}/>}})
     }
     </>
     }
