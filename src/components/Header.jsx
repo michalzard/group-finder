94cdbf0a5d5382cbedfc3ed86eb6574365846c7e
MichalZard <miso.platko@gmail.com>
2022-11-18 23:55:50 +0100

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import GroupIcon from '@mui/icons-material/Group';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Avatar, Typography,Menu,MenuItem, useMediaQuery } from '@mui/material';
 import GroupsLogo from "../assets/groups-logo.png";
 import "./Header.scss";
@@ -10,12 +10,16 @@ import { useDispatch } from 'react-redux';
 import { userLogout } from '../redux/reducers/userReducers';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import PersonIcon from '@mui/icons-material/Person';
+import PestControlIcon from '@mui/icons-material/PestControl';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
 
 
 function Header() {
   const [menuAnchor,setMenuAnchor] = useState(null);
   const dispatch = useDispatch();
   const isMobile = useMediaQuery("(max-width:600px)");
+  const navigate = useNavigate();
 
   return (
     <header>
@@ -27,6 +31,7 @@ function Header() {
     {isMobile ? null : <section className="logo"><img src={GroupsLogo} alt="Groups Logo"/></section>}
 
     <section className="menu">
+    <AddCircleOutlineIcon className='plus' onClick={()=>navigate("/profilecreation")}/>
     <Avatar onClick={(e)=>{setMenuAnchor(e.currentTarget)}}/>
     </section>
     <Menu
@@ -35,9 +40,9 @@ function Header() {
     onClose={()=>{setMenuAnchor(null);}}
     disableScrollLock={true}
     >
-      <MenuItem><PersonIcon/>My Profile</MenuItem>
+      <MenuItem><PersonIcon/>Edit my profile</MenuItem>
       <MenuItem>1</MenuItem>
-      <MenuItem>2</MenuItem>
+      <MenuItem><PestControlIcon/>Report a bug</MenuItem>
       <MenuItem onClick={()=>{dispatch(userLogout());}}><ExitToAppIcon/>Logout</MenuItem>
     </Menu>
     </header>
