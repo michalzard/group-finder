4a9c5b1c7428ee6a9c04d38f94aa8bd50d02de83
MichalZard <miso.platko@gmail.com>
2022-11-20 02:18:27 +0100

    changed right side of navbar to better convey idea of adding g.profiles and menu itself for user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,17 @@ import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import GroupIcon from '@mui/icons-material/Group';
 import { Link, useNavigate } from 'react-router-dom';
-import { Avatar, Typography,Menu,MenuItem, useMediaQuery } from '@mui/material';
+import { Typography,Menu,MenuItem, useMediaQuery } from '@mui/material';
 import GroupsLogo from "../assets/groups-logo.png";
 import "./Header.scss";
 import { useDispatch } from 'react-redux';
 import { userLogout } from '../redux/reducers/userReducers';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import PersonIcon from '@mui/icons-material/Person';
-import PestControlIcon from '@mui/icons-material/PestControl';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-
-
+import HelpIcon from '@mui/icons-material/Help';
+import FeedbackIcon from '@mui/icons-material/Feedback';
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
 
 function Header() {
   const [menuAnchor,setMenuAnchor] = useState(null);
@@ -32,17 +32,19 @@ function Header() {
 
     <section className="menu">
     <AddCircleOutlineIcon className='plus' onClick={()=>navigate("/profilecreation")}/>
-    <Avatar onClick={(e)=>{setMenuAnchor(e.currentTarget)}}/>
+    <DensityMediumIcon onClick={(e)=>{setMenuAnchor(e.currentTarget)}}/>
     </section>
     <Menu
     anchorEl={menuAnchor}
     open={Boolean(menuAnchor)}
     onClose={()=>{setMenuAnchor(null);}}
     disableScrollLock={true}
+    id="userProfileMenu"
     >
-      <MenuItem><PersonIcon/>Edit my profile</MenuItem>
-      <MenuItem>1</MenuItem>
-      <MenuItem><PestControlIcon/>Report a bug</MenuItem>
+      <MenuItem onClick={()=>navigate("/profile")}><PersonIcon/>Edit my profile</MenuItem>
+      {/* Popup dialog window with form to submit feedback */}
+      <MenuItem><FeedbackIcon/>Submit a feedback</MenuItem>
+      <MenuItem onClick={()=>navigate("help")}><HelpIcon/> Help / Faq</MenuItem>
       <MenuItem onClick={()=>{dispatch(userLogout());}}><ExitToAppIcon/>Logout</MenuItem>
     </Menu>
     </header>
