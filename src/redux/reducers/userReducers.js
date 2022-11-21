ea58d50fc702b82d5b05196196b4fb7bd786c6e8
MichalZard <miso.platko@gmail.com>
2022-11-21 01:13:05 +0100

    refactor

--- src/redux/reducers/userReducers.js
+++ src/redux/reducers/userReducers.js
@@ -5,7 +5,11 @@ export const userLogin = createAsyncThunk(
   "Auth/Login",
   async ({ username, password }, thunkApi) => {
     return await axios
-      .post(`${process.env.REACT_APP_API_URL}/auth/login`, { username, password },{withCredentials:true})
+      .post(
+        `${process.env.REACT_APP_API_URL}/auth/login`,
+        { username, password },
+        { withCredentials: true }
+      )
       .then((data) => {
         const { message, user } = data.data;
         const { email, username, id } = user;
@@ -22,10 +26,12 @@ export const userSession = createAsyncThunk(
   "Auth/Session",
   async (_, thunkApi) => {
     return await axios
-      .get(`${process.env.REACT_APP_API_URL}/auth/session`,{withCredentials:true})
+      .get(`${process.env.REACT_APP_API_URL}/auth/session`, {
+        withCredentials: true,
+      })
       .then((data) => {
-        const {message,user} = data.data;
-        return {message,user};
+        const { message, user } = data.data;
+        return { message, user };
       })
       .catch((err) => {
         return thunkApi.rejectWithValue(err.response.data.message);
@@ -35,20 +41,63 @@ export const userSession = createAsyncThunk(
 
 export const userRegister = createAsyncThunk(
   "Auth/Register",
-  async ({email,username,password},thunkApi) =>{
+  async ({ email, username, password }, thunkApi) => {
     // return register logic
-    return await axios.post(`${process.env.REACT_APP_API_URL}/auth/register`,{email,username,password},{withCredentials:true})
-    .then((data)=>{
-      const {message,user} = data.data;
-      return {message,user};
-    }).catch(err=>{console.log(err);return thunkApi.rejectWithValue(err.response.data.message)});
+    return await axios
+      .post(
+        `${process.env.REACT_APP_API_URL}/auth/register`,
+        { email, username, password },
+        { withCredentials: true }
+      )
+      .then((data) => {
+        const { message, user } = data.data;
+        return { message, user };
+      })
+      .catch((err) => {
+        console.log(err);
+        return thunkApi.rejectWithValue(err.response.data.message);
+      });
   }
 );
 
-
 export const userLogout = createAsyncThunk(
-  "Auth/Logout", async(_,thunkApi) => {
-    return await axios.get(`${process.env.REACT_APP_API_URL}/auth/logout`,{withCredentials:true})
-    .then(data=>{return data.data}).catch(err=>{return thunkApi.rejectWithValue(err.response.data.message);})
+  "Auth/Logout",
+  async (_, thunkApi) => {
+    return await axios
+      .get(`${process.env.REACT_APP_API_URL}/auth/logout`, {
+        withCredentials: true,
+      })
+      .then((data) => {
+        return data.data;
+      })
+      .catch((err) => {
+        return thunkApi.rejectWithValue(err.response.data.message);
+      });
   }
-)
+);
+
+export const userUpdate = createAsyncThunk(
+  "User/Update",
+  async (
+    {
+      /**email,birthday all that */
+    },
+    thunkApi
+  ) => {
+    return await axios
+      .patch(
+        `${process.env.REACT_APP_API_URL}/user/update`,
+        {},
+        {
+          withCredentials: true,
+        }
+      )
+      .then((data) => {
+        const {} = data.data;
+        return; //
+      })
+      .catch((err) => {
+        return thunkApi.rejectWithValue(err.response.data.message);
+      });
+  }
+);
