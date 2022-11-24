2ae0015b0c36594b44d21cb4a1747ff6580b22fa
MichalZard <miso.platko@gmail.com>
2022-11-24 02:08:11 +0100

    @TODO: accordions describing often asked questions

---
+++ src/components/pages/Faq.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import React from 'react'
+
+function Faq() {
+  return (
+    <main>
+    <Typography variant="h4" color="lightgray">TODO: Faq page full of accordions</Typography>
+    </main>
+    )
+}
+
+export default Faq;
