6bc22af87df6eaced93f6cd9a6099999a234e1df
MichalZard <miso.platko@gmail.com>
2022-11-29 21:48:06 +0100

    for easier asset use

---
+++ src/assets/valorant/ranks/rankData.js
@@ -0,0 +1,132 @@
+import {
+  valoAscendant1,
+  valoAscendant2,
+  valoAscendant3,
+  valoBronze1,
+  valoBronze2,
+  valoBronze3,
+  valoDiamond1,
+  valoDiamond2,
+  valoDiamond3,
+  valoGold1,
+  valoGold2,
+  valoGold3,
+  valoImmortal1,
+  valoImmortal2,
+  valoImmortal3,
+  valoIron1,
+  valoIron2,
+  valoIron3,
+  valoPlatinum1,
+  valoPlatinum2,
+  valoPlatinum3,
+  valoRadiant,
+  valoSilver1,
+  valoSilver2,
+  valoSilver3,
+} from "./index";
+
+const valorantRanks = [
+  {
+    src: valoIron1,
+    name: "iron1",
+  },
+  {
+    src: valoIron2,
+    name: "iron2",
+  },
+  {
+    src: valoIron3,
+    name: "iron3",
+  },
+  {
+    src: valoBronze1,
+    name: "bronze1",
+  },
+  {
+    src: valoBronze2,
+    name: "bronze2",
+  },
+  {
+    src: valoBronze3,
+    name: "bronze3",
+  },
+  {
+    src: valoSilver1,
+    name: "silver1",
+  },
+  {
+    src: valoSilver2,
+    name: "silver2",
+  },
+  {
+    src: valoSilver3,
+    name: "silver3",
+  },
+  {
+    src: valoGold1,
+    name: "gold1",
+  },
+  {
+    src: valoGold2,
+    name: "gold2",
+  },
+  {
+    src: valoGold3,
+    name: "gold3",
+  },
+  {
+    src: valoPlatinum1,
+    name: "plat1",
+  },
+  {
+    src: valoPlatinum2,
+    name: "plat2",
+  },
+  {
+    src: valoPlatinum3,
+    name: "plat3",
+  },
+  {
+    src: valoDiamond1,
+    name: "dia1",
+  },
+  {
+    src: valoDiamond2,
+    name: "dia2",
+  },
+  {
+    src: valoDiamond3,
+    name: "dia3",
+  },
+  {
+    src: valoAscendant1,
+    name: "asc1",
+  },
+  {
+    src: valoAscendant2,
+    name: "asc2",
+  },
+  {
+    src: valoAscendant3,
+    name: "asc3",
+  },
+  {
+    src: valoImmortal1,
+    name: "immo1",
+  },
+  {
+    src: valoImmortal2,
+    name: "immo2",
+  },
+  {
+    src: valoImmortal3,
+    name: "immo3",
+  },
+  {
+    src: valoRadiant,
+    name: "radiant",
+  },
+];
+
+export default valorantRanks;
